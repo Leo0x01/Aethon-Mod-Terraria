397dcfcf64584dd2ce7dd4a91bd0f9fbef05ce7a
Z User <z@container>
2026-09-02 18:55:29 +0000

    fix: CS0103 TextureAssets no existe en el contexto
    Agregado 'using Terraria.GameContent;' a LevelUpEventSystem.cs.
    TextureAssets (usado para MagicPixel en el overlay del evento) vive en
    ese namespace. Los 4 errores CS0103 eran las 4 llamadas a
    TextureAssets.MagicPixel.Value en DrawEventOverlay.

--- AethonMod/Content/Systems/LevelUpEventSystem.cs
+++ AethonMod/Content/Systems/LevelUpEventSystem.cs
@@ -2,6 +2,7 @@ using System.Collections.Generic;
 using Microsoft.Xna.Framework;
 using Microsoft.Xna.Framework.Graphics;
 using Terraria;
+using Terraria.GameContent;
 using Terraria.ID;
 using Terraria.ModLoader;
 using Terraria.UI;
